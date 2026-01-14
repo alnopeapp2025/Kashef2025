e0d5f655f021602db54f1a91fdd895d96104405e
alnopeapp2025 <alnopeapp2025@gmail.com>
2026-01-14 08:55:50 +0200

    Commit from Dualite Alpha:

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/Kashef2025/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
